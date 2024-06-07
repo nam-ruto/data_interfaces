a4765e70858bacfdcda5cd319feaf6746a324f81
namper <hoangducnam.work@gmail.com>
2024-06-07 15:07:21 +0700

    update

--- data.py
+++ data.py
@@ -1,6 +1,9 @@
 import pandas as pd
 import pymysql
 import pyodbc
+import logging
+
+logging.basicConfig(level=logging.INFO)
 
 class DataInterface:
     def __init__(self, db_type, connection_params):
@@ -9,20 +12,21 @@ class DataInterface:
         self.connect = None
     
     def connect_database(self):
-        try:
-            if self.db_type == "csv":
-                self.connect = None
-            elif self.db_type == "MySQL":
-                self.connect = pymysql.connect(**self.connection_params) # ** operator is used to unpacked the dictionary 
-            elif self.db_type == "SQL Sever":
-                self.connect = pyodbc.connect(self.connection_params)
-            else:
-                raise ValueError("Unsupported database type")
-            if self.connect:
-                print("Connected to " + self.db_type)
-        except Exception as e:
-            print(e)
-
+        if self.db_type == "csv":
+            self.connect = None
+        elif self.db_type == "MySQL":
+            self.connect = pymysql.connect(**self.connection_params) # ** operator is used to unpacked the dictionary 
+        elif self.db_type == "SQL Sever":
+            self.connect = pyodbc.connect(self.connection_params)
+        else:
+            logging.info("Unsupported database type")
+            return 0
+        # Logging: Connection
+        if self.connect:
+            logging.info("Connected to " + self.db_type)
+        else:
+            logging.error("Can not connect to {self.db_type}")
+             
     def read(self, query_or_path):
         if self.db_type in ["MySQL", "SQL Sever"]:
             cursor = self.connect.cursor()
@@ -36,16 +40,22 @@ class DataInterface:
         
     def create_table(self, table_name, columns):
         cursor = self.connect.cursor()
-        formatted_columns = [f"`{col}`" for col in columns]
-        query = f"CREATE TABLE {table_name} ({', '.join(columns)})"
+        if self.db_type == "My SQL":
+            query = f"CREATE TABLE IF NOT EXISTS {table_name} ({', '.join(columns)})"
+        elif self.db_type == "SQL Sever":
+            query = f"IF NOT EXISTS (SELECT * FROM sys.tables WHERE name = '{table_name}') BEGIN CREATE TABLE dbo.{table_name} ({', '.join(columns)}); END;"
         print("Executing query:", query)
         cursor.execute(query)
         self.connect.commit()
 
-    def write_to_mysql(self, data, table_name):
+    def write(self, data, table_name):
         cursor = self.connect.cursor()
-        query = f"INSERT INTO {table_name} VALUES (%s, %s, %s, %s)"
+        if self.db_type == "My SQL":
+            query = f"INSERT INTO {table_name} VALUES (%s, %s, %s, %s)"
+        elif self.db_type == "SQL Sever":
+            query = f"INSERT INTO {table_name} VALUES (?, ?, ?, ?)"
         cursor.executemany(query, data)
+        logging.info("Executed query successfully")
         self.connect.commit()    
 
     def close(self):
@@ -64,15 +74,26 @@ def MySQL(mysql_params):
     df, data = database.read("SELECT * FROM student")
     print(df) # Print dataframe into the console screen
 
-    table_name = "student_copy_2"
+    table_name = "student_copy_3"
     columns = ["id INT PRIMARY KEY", "name VARCHAR(50)", "gender VARCHAR(20)", "age INT"]
     database.create_table(table_name, columns)
-    database.write_to_mysql(data, table_name)
+    database.write(data, table_name)
+
+    database.close()
 
 def sql_sever(sql_sever_paramas):
     database = DataInterface("SQL Sever", sql_sever_paramas)
     database.connect_database()
-    print(database.read("SELECT * FROM student"))
+
+    df, data = database.read("SELECT * FROM student")
+    print(df)
+
+    table_name = "student_copy_4"
+    columns = ["id INT PRIMARY KEY", "name VARCHAR(50)", "gender VARCHAR(20)", "age INT"]
+    database.create_table(table_name, columns)
+    database.write(data, table_name)
+
+    database.close()
 
 
 
@@ -87,7 +108,7 @@ if __name__ == "__main__":
         'password': '1234',
         'db': 'my_database'
     }
-    MySQL(mysql_params)
+    # MySQL(mysql_params)
 
     # SQL Sever
     connection_string = (
@@ -95,4 +116,4 @@ if __name__ == "__main__":
     r"SERVER=DESKTOP-I5TALT9\NAMRUTO;"
     r"DATABASE=my_database"
     )
-    # sql_sever(connection_string)
+    sql_sever(connection_string)
